8c4e38122e8e3d504461464605e297e89ebe9508
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2025-03-24 09:48:09 -0400

    Create helper.h

---
+++ helper.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void test_func();
+
+//TODO implement these functions
+void calc_box_volume();
+void calc_count_digits();
+void reverse_int();
+void calc_sum();
+void calc_sqrt();
+
+void test_func(){cout<<"works";}
